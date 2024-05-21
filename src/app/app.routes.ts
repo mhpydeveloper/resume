dad71724d1120641961f21b67ce7181360ddf681
mhpydev <mh@mh.mh>
2024-05-21 20:30:04 +0330

    routing added to this project pages:home,projects,education

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {EducationComponent} from "./education/education.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'education', component: EducationComponent },
+];
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterOutlet
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
